09523b48d702635829dd7e1f3b416675c5ee3b5b
Mohamed Alkawari <mkhk110795@gmail.com>
2024-11-26 11:09:18 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 echo "Deploying project"
-
-source ~/.bashrc
 npx sst deploy --stage devops-coca --cloudformation-execution-policies arn:aws:iam::600627328431:policy/cdk_toolkit_least_privilege
-nohup dockerd &
-docker version
 npm install
-npm audit fix 
 npm audit fix 
 npx sst deploy --stage prod --cloudformation-execution-policies arn:aws:iam::600627328431:policy/cdk_toolkit_least_privilege
